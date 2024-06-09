113406bbf6eb409d44e56a83ea7d8f11ba691269
AstrooBot <129632296+AstrooBot@users.noreply.github.com>
2024-06-09 17:45:37 -0500

    uploading shortcout browser

---
+++ browers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+flag=$1
+
+if [ $flag == "-l" ]
+then
+	addr='https://'$2
+	firefox $addr
+
+else
+ 	for data in $@
+ 	do
+		addr=$addr$data+
+	done
+	firefox https://duckduckgo.com/?q=$addr&t=ffab&ia=web
+
+fi
+
+
+
