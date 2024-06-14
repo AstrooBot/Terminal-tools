ce969962ab9102c78792f6a35096197022a704fc
AstrooBot <129632296+AstrooBot@users.noreply.github.com>
2024-06-13 22:31:45 -0500

    Update browers.sh

--- browers.sh
+++ browers.sh
@@ -6,6 +6,14 @@ if [ $flag == "-l" ]
 then
 	addr='https://'$2
 	firefox $addr
+ 
+elif [ $flag == "-y" ]
+then
+ 	for data in ${@:2}
+ 	do
+		addr=$addr$data+
+	done
+	firefox https://www.youtube.com/results?search_query=$addr
 
 else
  	for data in $@
